4c84ef76b94aa7ec071c05f1cddddcc8e9077d05
Phu Phan <phu.phan@vinasource.com>
2017-07-25 17:05:12 +0700

    Implement a Queue using two Stack

--- practices/02-stacks-queues/rl-queue.js
+++ practices/02-stacks-queues/rl-queue.js
@@ -1,7 +1,3 @@
-// Replace my code with code in my class
-// My code looks like shit
-// Oh my god, why do not I learn it sooner?
-
 function Queue(capacity) {
   this._capacity = capacity || Infinity;
   this._storage = {};
@@ -23,17 +19,6 @@ Queue.prototype.enqueue = function(value) {
   }
 
   return 'Max _capacity already reached. Remove element before adding a new one.';
-  /*
-  if (this.count() >= this._capacity) {
-    var msgError = 'Max _capacity already reached. Remove element before adding a new one.';
-    console.log(msgError);
-    return msgError;
-  }
-
-  var key = Date.now() * Math.random();
-  this._storage[key] = value;
-  return Object.keys(this._storage).length;
-  */
 };
 
 /**
@@ -45,15 +30,6 @@ Queue.prototype.dequeue = function() {
   delete this._storage[this._head];
   if (this._head < this._tail) this._head++;
   return element;
-
-  /*
-  var keys = Object.keys(this._storage);
-  var firstKey = keys[0];
-  var firstQueue = this._storage[firstKey];
-  delete this._storage[firstKey];
-
-  return firstQueue;
-  */
 };
 
 /**
@@ -62,10 +38,6 @@ Queue.prototype.dequeue = function() {
  */
 Queue.prototype.peek = function() {
   return this._storage[this._head];
-
-  /*var keys = Object.keys(this._storage);
-  var firstKey = keys[0];
-  return this._storage[firstKey];*/
 };
 
 /**
@@ -73,10 +45,6 @@ Queue.prototype.peek = function() {
  */
 Queue.prototype.count = function() {
   return this._tail - this._head;
-
-  /*
-  return Object.keys(this._storage).length;
-  */
 };
 
 /**
@@ -89,15 +57,6 @@ Queue.prototype.contains = function(value) {
   }
 
   return false;
-  /*for (var key in this._storage) {
-    var storeValue = this._storage[key];
-    if (storeValue === value) {
-      return true;
-      break;
-    }
-  }
-
-  return false;*/
 };
 
 /**
@@ -111,21 +70,6 @@ Queue.prototype.until = function (value) {
   for (var i = this._head; i < this._tail; i++) {
     if (this._storage[i] === value) return i - this._head + 1;
   }
-
-  /*
-  var total = this.count();// 6
-  for (var key in this._storage) {
-    var storeValue = this._storage[key];
-    if (storeValue === value) {
-      return total;
-      break;
-    }
-
-    total--;
-  }
-
-  return -1;
-  */
 };
 
 
@@ -148,32 +92,129 @@ console.log(myQueue.until('e'), 'should be 3');
 console.log(myQueue);
 
 
-/*
-myQueue.enqueue(9);
-myQueue.enqueue(6);
-myQueue.enqueue(3);
-//myQueue.dequeue();
-//myQueue.dequeue();
-console.log(myQueue.enqueue(7));
-console.log(myQueue.enqueue(5));
-myQueue.enqueue(2);
-//myQueue.dequeue();
-//myQueue.dequeue();
-//myQueue.dequeue();
-//myQueue.dequeue();
+function Stack(capacity) {
+  this._capacity = capacity || Infinity;
+  this._storage = {};
+  this._count = 0;
+}
+
+/**
+ * => count of stack
+ * add value to collection
+ * 0(1)
+ * @param value
+ */
+Stack.prototype.push = function(value) {
+  if (this._count < this._capacity) {
+    this._storage[this._count++] = value;
+    return this._count;
+  }
+
+  return 'Max _capacity already reached. Remove element before adding a new one.';
+};
+
+/**
+ * => most recent element added collection
+ * Remove item so that it is no longer in collection
+ * 2 items, {0: ..., 1: ...}
+ */
+Stack.prototype.pop = function() {
+  if (this._count === 0) {
+    return 'No element inside the stack. Add element before poping.';
+  }
+
+  var value = this._storage[--this._count];
+  delete this._storage[this._count];
+  return value;
+};
+
+/**
+ * => most recent element added collection
+ * Similiar to pop, but do not remove element from collection
+ */
+Stack.prototype.peek = function() {
+  return this._storage[this._count - 1];
+};
+
+/**
+ * => number of elements in stack
+ * @returns {Number}
+ */
+Stack.prototype.count = function() {
+  return this._count;
+};
+
+/**
+ * Check if a value is in the stack:
+ * @param value
+ */
+Stack.prototype.contains = function(value) {
+  for (var i = 0; i < this._count; i++) {
+    if (value === this._storage[i]) return true;
+  }
+
+  return false;
+};
+
+/**
+ * Get the number of pops until you get to a certain value
+ * stack values - (first)c-d-e-f-g-h(last)
+ * myStack.until('f')
+ * => 4
+ * @param value
+ */
+Stack.prototype.until = function(value) {
+  // (first) c -> d -> e -> f -> g -> h (last)
+  for (var i = 0; i < this._count; i++) {
+    if (this._storage[i] === value) return i + 1;
+  }
+
+  return null;
+};
+
+function Queue_TwoStacks() {
+  this._stackIn = new Stack();// [1,2,3,4,5]
+  this._stackOut = new Stack();// [5,4,3,2,1] => pop() => 1
+}
+
+Queue_TwoStacks.prototype.enqueue = function(val) {
+  this._stackIn.push(val);
+};
+
+Queue_TwoStacks.prototype._transferStacks = function() {
+  while (this._stackIn.count() > 0) {// [5,4,3,2,1]
+    this._stackOut.push(this._stackIn.pop());
+  }
+};
+
+Queue_TwoStacks.prototype.dequeue = function() {
+  if (this._stackOut.count() === 0) this._transferStacks();
+  return this._stackOut.pop();// [5,4,3,2,1]
+};
+
+Queue_TwoStacks.prototype.count = function() {
+  return this._stackIn.count() + this._stackOut.count();
+};
+
+Queue_TwoStacks.prototype.peek = function() {
+  if (this._stackOut.count() === 0) this._transferStacks();
+  return this._stackOut.peek();
+};
 
 
-console.log(Object.values(myQueue._storage));
+console.log('');
+console.log('');
+console.log('____________________________________________');
+console.log('Implement a queue using two stacks.');
 
-console.log('=== peek');
-console.log(myQueue.peek());
+var myQueue_TwoStacks = new Queue(3);
+console.log(myQueue_TwoStacks.enqueue('a'), 'should be 1');
+console.log(myQueue_TwoStacks.enqueue('b'), 'should be 2');
+console.log(myQueue_TwoStacks.enqueue('c'), 'should be 3');
+console.log(myQueue_TwoStacks.enqueue('d'), 'should be Max capacity reached');
+console.log(myQueue_TwoStacks.dequeue(), 'should be a');
+console.log(myQueue_TwoStacks.count(), 'should be 2');
+console.log(myQueue_TwoStacks.peek(), 'should be b');
+console.log(myQueue_TwoStacks.count(), 'should be 2');
 
-console.log('=== count');
-console.log(myQueue._storage);
 
-console.log('=== contains');
-console.log(myQueue.contains(0));
-
-console.log('=== until');
-console.log(myQueue.until(6));
-*/
